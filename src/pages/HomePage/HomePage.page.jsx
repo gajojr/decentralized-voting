3ccb591a05502caedcd98b57365ac32208edd342
Andrija Gajic <andrijagajicbusiness@gmail.com>
2022-09-15 17:59:14 +0200

    fix: check if there is ethereum environment and check if it's rinkeby on home page

--- src/pages/HomePage/HomePage.page.jsx
+++ src/pages/HomePage/HomePage.page.jsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
 	PageWrapper,
-	Main
+	Main,
+	Alert,
+	AlertCaption,
+	AlertText,
+	AlertBtn
 } from './HomePage.style';
 import Header from '../../components/Header/Header.component';
 import Intro from '../../components/HomePageComponents/Intro/Intro.component';
 import CandidatesList from '../../components/HomePageComponents/CandidatesList/CandidatesList.component';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+
+const ReactAlert = withReactContent(Swal);
 
 const HomePage = () => {
+	useEffect(() => {
+		if (!window.ethereum) {
+			ReactAlert.fire({
+				html: <Alert>
+					<AlertCaption>Wallet notification</AlertCaption>
+					<AlertText>You don't have any ethereum wallet installed</AlertText>
+					<AlertBtn onClick={ReactAlert.close}>OK</AlertBtn>
+				</Alert>,
+				showConfirmButton: false,
+				background: '#152D25'
+			});
+
+			return;
+		}
+
+		const chainId = window.ethereum.networkVersion;
+		if (chainId !== "4") {
+			ReactAlert.fire({
+				html: <Alert>
+					<AlertCaption>Wallet notification</AlertCaption>
+					<AlertText>Switch network to rinkeby and try again</AlertText>
+					<AlertBtn onClick={ReactAlert.close}>OK</AlertBtn>
+				</Alert>,
+				showConfirmButton: false,
+				background: '#152D25'
+			});
+
+			return;
+		}
+	}, []);
+
 	return (
 		<PageWrapper>
 			<Header />
--- src/pages/HomePage/HomePage.style.js
+++ src/pages/HomePage/HomePage.style.js
@@ -14,3 +14,34 @@ export const Main = styled.main `
 	flex-direction: column;
 	align-items: center;
 `;
+
+export const Alert = styled.div `
+	background-color: #152D25;
+	border-radius: 4px;
+	color: #fff;
+`;
+
+export const AlertCaption = styled.h2 `
+
+`;
+
+export const AlertText = styled.p `
+	
+`;
+
+export const AlertBtn = styled.button `
+	background: #00FFA7;
+	box-shadow: 0px 0px 15px rgba(2, 255, 168, 0.4);
+	border-radius: 3px;
+	border: none;
+	color: #000;
+	padding: 10px 25px;
+	margin-bottom: 20px;
+	font-size: 1.3rem;
+	font-weight: 700;
+	font-family: 'Urbanist', sans-serif;
+
+	&:hover {
+		cursor: pointer;
+	}
+`;
